d2da8f742250d893e9525a61231584e2633f88e8
Soniya Faroda <143260442+soniyafaroda@users.noreply.github.com>
2025-11-26 23:20:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
+// --------------------------
+// Person Class
+// --------------------------
 class Person {
-  private _name: string;
-  private _age: number;
-
-  constructor(name: string, age: number) {
+  constructor(name, age) {
     this._name = name;
     this._age = age;
   }
 
   // Getter for name
-  get name(): string {
+  get name() {
     return this._name;
   }
 
-  // Setter for age
-  set age(age: number) {
-    this._age = age;
+  // Getter for age
+  get age() {
+    return this._age;
   }
 
-  // Getter for age (not required, but useful for test case output)
-  get age(): number {
-    return this._age;
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
   }
 }
 
+// --------------------------
+// Student Class (inherits Person)
+// --------------------------
 class Student extends Person {
-  constructor(name: string, age: number) {
-    super(name, age);
-  }
-
-  study(): void {
+  study() {
     console.log(`${this.name} is studying`);
   }
 }
 
+// --------------------------
+// Teacher Class (inherits Person)
+// --------------------------
 class Teacher extends Person {
-  constructor(name: string, age: number) {
-    super(name, age);
-  }
-
-  teach(): void {
+  teach() {
     console.log(`${this.name} is teaching`);
   }
 }
+
+// --------------------------
+// Do not change the code below this line
+// --------------------------
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
