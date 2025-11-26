7496a97cdd8187d679622307a56c26a325a40edc
Soniya Faroda <143260442+soniyafaroda@users.noreply.github.com>
2025-11-26 22:51:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,85 +1,44 @@
 class Person {
-  constructor(name, age) {
-    this._name = name; // private-style variable (by convention)
+  private _name: string;
+  private _age: number;
+
+  constructor(name: string, age: number) {
+    this._name = name;
     this._age = age;
   }
 
   // Getter for name
-  get name() {
+  get name(): string {
     return this._name;
   }
 
-  // Getter for age
-  get age() {
-    return this._age;
+  // Setter for age
+  set age(age: number) {
+    this._age = age;
   }
 
-  // Setter for age
-  set age(newAge) {
-    this._age = newAge;
+  // Getter for age (not required, but useful for test case output)
+  get age(): number {
+    return this._age;
   }
 }
-// Subclass: Student (inherits from Person)
+
 class Student extends Person {
-  study() {
+  constructor(name: string, age: number) {
+    super(name, age);
+  }
+
+  study(): void {
     console.log(`${this.name} is studying`);
   }
 }
 
-// Subclass: Teacher (inherits from Person)
 class Teacher extends Person {
-  teach() {
+  constructor(name: string, age: number) {
+    super(name, age);
+  }
+
+  teach(): void {
     console.log(`${this.name} is teaching`);
   }
 }
-// --------------------------
-//  Example usage and output
-// --------------------------
-
-// Creating a Person instance
-const person = new Person("John", 25);
-console.log(person.name);  // Output: John
-
-person.age = 30;  // Using setter to change the age
-console.log(person.age);  // Output: 30
-
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study();  // Output: Alice is studying
-
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach();  // Output: Bob is teaching
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//complete this code
-class Person {}
-
-class Student extends Person {}
-
-class Teacher extends Person {}
-
-// Do not change the code below this line
-window.Person = person()
-window.student= Student ()
-window.teacher= teacher ()
